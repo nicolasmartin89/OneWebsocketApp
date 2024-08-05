e6247f8b2ff8bce324fe295540c00c8323b7fa08
Nicolas Martin <nicolasmartin89@gmail.com>
2024-08-05 17:12:21 -0300

    Create WebSocketApp

---
+++ src/main/java/com/app/onewebsocketapp/OnewebsocketappApplication.java
@@ -0,0 +1,13 @@
+package com.app.onewebsocketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnewebsocketappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnewebsocketappApplication.class, args);
+	}
+
+}
